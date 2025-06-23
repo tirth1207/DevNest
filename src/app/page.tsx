d36bfc2d2e7d893be9500b4be0502e1430867b2c
Tirth Rathod <rathodtirth1207@gmail.com>
2025-06-23 09:01:46 +0530

    fixed_main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ async function signInWithGithub() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'github',
     options: {
-      redirectTo: `${window.location.origin}/dashboard`,
+      redirectTo: `/dashboard`,
     },
   })
 }
