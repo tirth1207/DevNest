ebf9dbe13acfbf8324e4ce05c5c27ff223f147f9
Tirth <104121248+tirth1207@users.noreply.github.com>
2025-09-28 10:50:41 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ async function signInWithGithub() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "github",
     options: {
-      redirectTo: `localhost:3000/dashboard`,
+      redirectTo: `/dashboard`,
     },
   })
 }
